9a01fe3e2ebdd4ecc4098f63659857d3dc2dd805
Malcolm Tan <malcolmtan2003@gmail.com>
2026-01-30 00:38:32 +0800

    Added add. list functions

--- src/main/java/Fish.java
+++ src/main/java/Fish.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Fish {
-    public static final String bar = "    ____________________________________________________________\n";
+    public static final String BAR = "    ____________________________________________________________\n";
     public static final String INTRO = """
                 ____________________________________________________________
                 Hello! I'm the emperor 
@@ -14,17 +14,22 @@ public class Fish {
                 \\_)|  .--',|  |_.'.-._)   \\|  .-.  |\s
                   \\|  |_)(_|  |   \\       /|  | |  |\s
                    `--'    `--'    `-----' `--' `--'\s
-                   
+            
                 Why are you repeating after me?
                 ____________________________________________________________
             """;
 
     public static final String BYE = """
                 ____________________________________________________________
+                Goodbye land dweller
                 ***swims away***
                 ____________________________________________________________
             """;
 
+    private static String[] list;
+    private static int listLength;
+
+    // Level-1
     public static String readInput() {
         String line;
         Scanner in = new Scanner(System.in);
@@ -33,9 +38,7 @@ public class Fish {
         return line;
     }
 
-    public static void main(String[] args) {
-
-        System.out.println(INTRO);
+    public static void echo() {
         boolean isActive = true;
 
         while (isActive) {
@@ -44,8 +47,49 @@ public class Fish {
                 isActive = false;
                 break;
             }
-            System.out.println(bar + "    " + line + "\n" + bar);
+            System.out.println(BAR + "    " + line + "\n" + BAR);
         }
+    }
+
+    // Level-2
+    public static void addToList(String item) {
+        list[listLength] = item;
+        listLength++;
+        System.out.println(BAR + "    added: " + item + "\n" + BAR);
+    }
+
+    public static void printList() {
+        System.out.print(BAR);
+        for (int i = 0; i < listLength; i++) {
+            System.out.println("    " + (i + 1) + ". " + list[i]);
+        }
+        System.out.println(BAR);
+    }
+
+    public static void performListOps() {
+        list = new String[100];
+        listLength = 0;
+        boolean isActive = true;
+
+        while (isActive) {
+            String line = readInput().trim();
+            if (line.equals("bye")) {
+                isActive = false;
+                break;
+            } else if (line.equals("list")) {
+                printList();
+            }
+            else {
+                addToList(line);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(INTRO);
+
+        performListOps();
         System.out.println(BYE);
     }
 }
