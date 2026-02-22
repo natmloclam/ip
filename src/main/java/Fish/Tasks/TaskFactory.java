f8d806f760172d77bccb7eb6d9d6b81f4af8218a
Malcolm Tan <malcolmtan2003@gmail.com>
2026-02-22 17:10:35 +0800

    Extract Task creation methods into new TaskFactory.java

---
+++ src/main/java/Fish/Tasks/TaskFactory.java
@@ -0,0 +1,67 @@
+package Fish.Tasks;
+
+import Fish.Helpers.FishException;
+import Fish.Helpers.FishMessages;
+
+public class TaskFactory {
+    public static Task createTask(String taskType, String arg) throws FishException {
+        return switch (taskType) {
+            case TaskList.TASK_TYPE_TODO -> createTodo(arg);
+            case TaskList.TASK_TYPE_DEADLINE -> createDeadline(arg);
+            case TaskList.TASK_TYPE_EVENT -> createEvent(arg);
+            default -> {
+                System.out.println(taskType + " is not a valid command!");
+                throw new FishException(FishMessages.INVALID_COMMAND);
+            }
+        };
+    }
+
+    public static Task createTodo(String input) throws FishException {
+        if (input.isEmpty()) {
+            throw new FishException(FishMessages.INVALID_TODO);
+        }
+        return new Todo(input);
+    }
+
+    public static Task createDeadline(String input) throws FishException {
+        // get index of "/by"
+        int deadlineByPosition = input.indexOf("/by");
+
+        // throw exception if no "/by"
+        if (deadlineByPosition == -1) {
+            throw new FishException(FishMessages.INVALID_DEADLINE);
+        }
+
+        String description = input.substring(0, deadlineByPosition).strip();
+        String deadline = input.substring(deadlineByPosition + 3).strip();
+
+        // throw exception if description/deadline is empty
+        if (description.isEmpty() || deadline.isEmpty()) {
+            throw new FishException(FishMessages.INVALID_DEADLINE);
+        }
+
+        return new Deadline(description, deadline);
+    }
+
+    public static Task createEvent(String input) throws FishException {
+        // get indices of "/from" and "/to"
+        int eventFromPosition = input.indexOf("/from");
+        int eventToPosition = input.indexOf("/to");
+
+        // throw exception if "/from" or "/to" is missing
+        if (eventFromPosition == -1 || eventToPosition == -1) {
+            throw new FishException(FishMessages.INVALID_EVENT);
+        }
+
+        String description = input.substring(0, eventFromPosition).strip();
+        String from = input.substring(eventFromPosition + 5, eventToPosition).strip();
+        String to = input.substring(eventToPosition + 3).strip();
+
+        // throw exception if description/start/end time is missing
+        if (description.isEmpty() || from.isEmpty() || to.isEmpty()) {
+            throw new FishException(FishMessages.INVALID_EVENT);
+        }
+
+        return new Event(description, from, to);
+    }
+}
--- src/main/java/Fish/Tasks/TaskList.java
+++ src/main/java/Fish/Tasks/TaskList.java
@@ -11,7 +11,7 @@ public class TaskList {
     public static final String TASK_TYPE_EVENT = "event";
     public static final String TASK_TYPE_DEADLINE = "deadline";
 
-    private ArrayList<Task> tasks;
+    private final ArrayList<Task> tasks;
 
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
@@ -69,76 +69,14 @@ public class TaskList {
         tasks.remove(indexToDelete);
     }
 
-    // ========= CREATE TASKS METHODS ========= //
-    public void createNewDeadline(String input) throws FishException {
-        // get index of "/by"
-        int deadlineByPosition = input.indexOf("/by");
-
-        // throw exception if no "/by"
-        if (deadlineByPosition == -1) {
-            throw new FishException(FishMessages.INVALID_DEADLINE);
-        }
-
-        String description = input.substring(0, deadlineByPosition).strip();
-        String deadline = input.substring(deadlineByPosition + 3).strip();
-
-        // throw exception if description/deadline is empty
-        if (description.isEmpty() || deadline.isEmpty()) {
-            throw new FishException(FishMessages.INVALID_DEADLINE);
-        }
-
-        tasks.add(new Deadline(description, deadline));
-    }
-
-    public void createNewEvent(String input) throws FishException {
-        // get indices of "/from" and "/to"
-        int eventFromPosition = input.indexOf("/from");
-        int eventToPosition = input.indexOf("/to");
-
-        // throw exception if "/from" or "/to" is missing
-        if (eventFromPosition == -1 || eventToPosition == -1) {
-            throw new FishException(FishMessages.INVALID_EVENT);
-        }
-
-        String description = input.substring(0, eventFromPosition).strip();
-        String from = input.substring(eventFromPosition + 5, eventToPosition).strip();
-        String to = input.substring(eventToPosition + 3).strip();
-
-        // throw exception if description/start/end time is missing
-        if (description.isEmpty() || from.isEmpty() || to.isEmpty()) {
-            throw new FishException(FishMessages.INVALID_EVENT);
-        }
-
-        tasks.add(new Event(description, from, to));
-    }
-
-    public void createNewTodo(String input) throws FishException {
-        if (input.isEmpty()) {
-            throw new FishException(FishMessages.INVALID_TODO);
-        }
-        tasks.add(new Todo(input));
-    }
-
     public int getTaskIndex(String input) {
         return Integer.parseInt(input) - 1;
     }
 
     // ========= HIGHER LEVEL FUNCTIONS ========= //
     public void addToList(String command, String item) throws FishException {
-        switch (command) {
-        case TASK_TYPE_TODO:
-            createNewTodo(item);
-            break;
-        case TASK_TYPE_DEADLINE:
-            createNewDeadline(item);
-            break;
-        case TASK_TYPE_EVENT:
-            createNewEvent(item);
-            break;
-        default:
-            System.out.println(command + " is not a valid command!");
-            throw new FishException(FishMessages.INVALID_COMMAND);
-        }
+        Task task = TaskFactory.createTask(command, item);
+        tasks.add(task);
         Printer.printAddItemMessage(this);
     }
 }
